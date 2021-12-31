be738442de1cbbb6a6d7f265452311f7b42fd481
kilhyeonjun <kboxstar@gmail.com>
2021-12-31 09:32:00 +0900

    2501 약수구하기 v1

--- 2501/main.java
+++ 2501/main.java
@@ -1,5 +1,30 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 class Main{ 
     public static void main(String[] args){ 
-        System.out.println("Hello World!"); 
-    } 
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            System.out.println(getMeasureByOrder(n, k)); 
+        } catch (Exception e) {
+            //TODO: handle exception
+        }
+        
+    }
+    public static int getMeasureByOrder(int n, int k){
+        int cnt = 0;
+        for(int i=1; i<=n; i++){
+            if(n%i == 0){
+                cnt++;
+                if(cnt == k){
+                    return i;
+                }
+            }
+        }
+        return 0;
+    }
 }
