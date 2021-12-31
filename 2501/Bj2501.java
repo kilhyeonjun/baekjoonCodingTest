347b057470e307a5269f468abc85a4d621ff9838
kilhyeonjun <kboxstar@gmail.com>
2021-12-31 09:58:41 +0900

    클래스명 변경 및 sourcePath 설정

--- 2501/Bj2501.java
+++ 2501/Bj2501.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-class Main{ 
+public class Bj2501{ 
     public static void main(String[] args){ 
         try {
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
