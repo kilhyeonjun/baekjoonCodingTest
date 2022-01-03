7ffdacf7ed3b4fc663513535684aa6fae9407dc5
kilhyeonjun <kboxstar@gmail.com>
2022-01-03 13:22:38 +0900

    11021 A+B - 7 v1

---
+++ 11021/Bj11021.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Bj11021 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int t = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= t; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(String.format("Case #%d: %d\n", i, (a + b)));
+        }
+        bw.flush();
+    }
+}
