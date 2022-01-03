5a187dcab0921ee8e0082262c45cafc85d1809e4
kilhyeonjun <kboxstar@gmail.com>
2022-01-03 11:55:13 +0900

    15552 빠른 A+B v1

---
+++ 15552/Bj15552.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Bj15552 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        int a, b;
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            a = Integer.parseInt(st.nextToken());
+            b = Integer.parseInt(st.nextToken());
+            bw.write((a + b) + "\n");
+        }
+        bw.flush();
+    }
+}
