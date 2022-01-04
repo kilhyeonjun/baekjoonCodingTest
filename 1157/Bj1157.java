ac8f79b1b090b1973e381109b776be8b552f0d86
kilhyeonjun <kboxstar@gmail.com>
2022-01-04 14:21:30 +0900

    1157 import 제거

--- 1157/Bj1157.java
+++ 1157/Bj1157.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Set;
 
 public class Bj1157 {
     public static void main(String[] args) throws Exception {
