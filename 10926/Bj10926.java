81e2de6cc69ca841bb79504eee1eaa6c2f8988dd
kilhyeonjun <kboxstar@gmail.com>
2022-01-26 17:23:20 +0900

    10926 ??! v1

---
+++ 10926/Bj10926.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Bj10926 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String id = br.readLine();
+        System.out.println(id + "??!");
+    }
+}
