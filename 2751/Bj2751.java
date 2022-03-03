f9b55f888d13cd3241fdeaab7242e9ef5aa1f6fa
kilhyeonjun <kboxstar@gmail.com>
2022-03-03 20:48:28 +0900

    2751 수 정렬하기 v2 cleanup

--- 2751/Bj2751.java
+++ 2751/Bj2751.java
@@ -43,10 +43,4 @@ public class Bj2751 {
             }
         }
     }
-
-    public static void printArray(int[] a) {
-        for (int i = 0; i < a.length; i++)
-            System.out.print(a[i] + " ");
-        System.out.println();
-    }
 }
