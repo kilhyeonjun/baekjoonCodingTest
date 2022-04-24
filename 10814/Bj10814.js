434b4d8ef84e1521c6f3e5063eccfeff08a583eb
kilhyeonjun <kboxstar@gmail.com>
2022-04-24 16:44:47 +0900

    10814 나이순 정렬 v2

--- 10814/Bj10814.js
+++ 10814/Bj10814.js
@@ -2,6 +2,11 @@ const fs = require('fs');
 const [n, ...arr] = fs.readFileSync(__dirname + "/example.txt").toString().trim().split('\n');
 // const [n, ...arr] = fs.readFileSync("/dev/stdin").toString().trim().split('\n');
 
+arr.sort((a, b)=> a.split(' ')[0] - b.split(' ')[0]);
+
+console.log(arr.join('\n'));
+
+/* 10814 나이순 정렬 v1
 const result = Array.from(Array(Number(n)), () => Array(2).fill(null))
 
 arr.map((v, i)=>{
@@ -12,3 +17,4 @@ arr.map((v, i)=>{
 result.sort((a, b) => a[0] - b[0])
 
 result.map((v)=>{console.log(v[0], v[1])})
+*/
